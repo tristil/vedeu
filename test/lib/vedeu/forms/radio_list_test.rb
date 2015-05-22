b4b0721fea81a7d7a4df9cba352ec8c9ed306b41
Gavin Laking <gavinlaking@gmail.com>
2015-05-22 23:48:21 +0100

    Add more tests.

--- test/lib/vedeu/forms/radio_list_test.rb
+++ test/lib/vedeu/forms/radio_list_test.rb
@@ -3,10 +3,12 @@ module Vedeu
   describe RadioList do
 
     let(:described) { Vedeu::RadioList }
-    let(:instance) { described.new }
+    let(:instance)  { described.new(options) }
+    let(:options)   {}
 
     describe '#initialize' do
       it { instance.must_be_instance_of(described) }
+      it { instance.instance_variable_get('@options').must_equal(options) }
     end
 
   end # RadioList
